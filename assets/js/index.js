22c55e3291bd83b5d036f516706a669c2461ad0d
abusaddique95 <abusaddique95@live.com>
2022-05-10 01:10:50 +0100

    added functions

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,15 +1,5 @@
-// const currentDay = document.getElementById("currentDay");
-// const mainSection = document.getElementById("main");
-
-// current date
-const renderDateSection = () => {
-  renderDate = moment().format("dd Do MMM yyyy");
-};
-
-// first function
-const onLoad = () => {};
-
-const workingHours = (workingHours = [
+// array
+const workingHours = [
   {
     label: "9AM",
     key: 9,
@@ -46,14 +36,48 @@ const workingHours = (workingHours = [
     label: "5PM",
     key: 17,
   },
-]);
+];
+
+// current date
+const renderDateSection = () => {
+  return moment().format("ddd Do MMM YYYY");
+  $("#currentDay").append(currentDate);
+};
+
+$("#currentDay").text(renderDateSection);
+
+// local storage
+const readFromLocalStorage = (key, defaultValue) => {
+    // get from LS using key name
+    const dataFromLS = localStorage.getItem(key);
+    // parse data from LS
+    const parsedData = JSON.parse(dataFromLS);
+    if (parsedData) {
+      return parsedData;
+    } else {
+      return defaultValue;
+    }
+  };
+  const writeToLocalStorage = (key, value) => {
+    // convert value to string
+    const stringifiedValue = JSON.stringify(value);
+    // set stringified value to LS for key name
+    localStorage.setItem(key, stringifiedValue);
+
+// first function
+const onLoad = () => {};
+
+
 
 const renderTimeblocks = () => {};
+
 // create and append timeblocks
 const timeBlock = $("#time-blocks");
+
 const renderTimeblock = () => {
   console.log(workingHours);
 };
+workingHours.forEach(renderTimeblock);
 
 const timeblock = `<div class="d-flex flow-row">
 <div class="col text-center">
@@ -69,6 +93,26 @@ const timeblock = `<div class="d-flex flow-row">
 </div>
 </div>`;
 workingHours.forEach(renderTimeblock);
+
+const saveButton = (event) => {
+    const button = $(event.target);
+    
+
+}
+
+const getName = (workingHours) => {
+    const currentHourBlock = moment().hour();
+    // if statement for past, present, future.
+
+    if (workingHours === currentHourBlock) {
+        return "present";
+    }
+
+        if (workingHours > currentHourBlock) {
+return "future"
+    }
+    else return "past"
+}
 
 const onReady = () => {
   console.log(onReady);
---
+++ practice.js
@@ -0,0 +1,94 @@
+const workingHours = [
+  { label: "9am", key: 9 },
+  { label: "10am", key: 10 },
+  { label: "11am", key: 11 },
+  { label: "12pm", key: 12 },
+  { label: "1pm", key: 13 },
+  { label: "2pm", key: 14 },
+  { label: "3pm", key: 15 },
+  { label: "4pm", key: 16 },
+  { label: "5pm", key: 17 },
+];
+//render date using moment.js in header
+const renderDate = () => {
+  const currentDate = moment().format("dddd,  Do of MMMM YYYY");
+  console.log(currentDate);
+  $("#currentDay").append(currentDate);
+};
+const readFromLocalStorage = (key, defaultValue) => {
+  // get from LS using key name
+  const dataFromLS = localStorage.getItem(key);
+  // parse data from LS
+  const parsedData = JSON.parse(dataFromLS);
+  if (parsedData) {
+    return parsedData;
+  } else {
+    return defaultValue;
+  }
+};
+const writeToLocalStorage = (key, value) => {
+  // convert value to string
+  const stringifiedValue = JSON.stringify(value);
+  // set stringified value to LS for key name
+  localStorage.setItem(key, stringifiedValue);
+};
+const getEventForTimeBlock = (workingHours) => {};
+const renderTimeBlocks = () => {
+  //for each working hour create and append time block time blocks
+  const timeBlocks = $("#time-block");
+  const renderTimeBlock = (workingHours) => {
+    console.log(workingHours);
+    //create time blocks dynamically
+    const timeBlock = `<div class="row p-2 future my 2 ${getClassName(
+      workingHours
+    )}">
+      <div class="col-md-1 col-sm-12 text-center my-1 d-flex flex-column justify-content-center">${
+        workingHours.label
+      }</div>
+      <textarea data-info=${
+        workingHours.key
+      } class="col-md-9 col-sm-12" rows="3">${getEventForTimeBlock(
+      workingHours.key
+    )}</textarea>
+      <div class="col-md-2 col-sm-12 text-center my-1 d-flex flex-column justify-content-center">
+        <button type="button" data-hour=${
+          workingHours.key
+        } class="btn btn-success">Save</button>
+      </div>`;
+    //append to parent time block
+    timeBlocks.append(timeBlock);
+  };
+  workingHours.forEach(renderTimeBlock);
+  timeBlocks.on("click", handleSave);
+};
+const handleSave = (event) => {
+  const target = $(event.target);
+  if (target.is("BUTTON")) {
+    const key = target.attr("data-hour");
+    const value = $(`textarea[data-info="${key}"]`).val().trim();
+    console.log(value);
+    const planner = readFromLocalStorage("planner", {});
+    planner[key] = value;
+    writeToLocalStorage("planner", planner);
+  }
+};
+const getClassName = (workingHours) => {
+  const currentHour = moment().hour();
+  //if workingHour is present
+  if (workingHours === currentHour) {
+    return "present";
+  }
+  //if workingHours is future
+  if (workingHours > currentHour) {
+    return "future";
+  }
+  //else workingHours is past
+  return "past";
+  //else past
+};
+//function to handle click on save buttons
+const onReady = () => {
+  renderDate();
+  renderTimeBlocks();
+};
+$(document).ready(onReady);
